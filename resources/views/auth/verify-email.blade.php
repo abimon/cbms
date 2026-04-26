8b90e47a2e412938e42bb578e40d185beda4096d
abimon <eddyabby59@gmail.com>
2026-04-26 15:40:58 +0300

    redo layouts

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -14,10 +14,12 @@
             {{ __('Before proceeding, please check your email for a verification link.') }}
             {{ __('If you did not receive the email') }},
         </div>
-        <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-            @csrf
-            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-        </form>
+        <div class="auth-body">
+            <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
+                @csrf
+                <button type="submit" class="btn btn-secondary p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+            </form>
+        </div>
     </div>
 </div>
 @endsection
